757039f65b04272c7e36b0a261236d26f5935b7b
toanLa <toananhle2k5@gmail.com>
2024-11-28 01:25:15 +0700

    update admin dm

--- admin/index.php
+++ admin/index.php
@@ -4,10 +4,60 @@ session_start();
 include "header.php";
 include "../model/pdo.php";
 include "../model/sanpham.php";
+include "../model/danhmuc.php";
 include "../global.php";
 if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
+         //quan ly danh muc
+         case 'add_dm': {
+            if (isset($_POST['submit']) && ($_POST['submit'])) {
+                $name = $_POST['name'];
+                $img = null;
+                $listone_dm = loadAll_danhmuc($name, 0);
+                if (is_array($listone_dm) && count($listone_dm) > 0) {
+                    $err = "Tên danh mục đã tồn tại";
+                } else {
+
+                    if ($_FILES['img']['name'] != "") {
+                        $img = time() . "_" . $_FILES['img']['name'];
+                        move_uploaded_file($_FILES['img']['tmp_name'], "../uploads/img_dm/$img");
+                    }
+                    insert_dm($name, $img);
+                    $thongbao = "Thêm danh mục thành công";
+                }
+            }
+            include "./danhmuc/add_dm.php";
+            break;
+        }
+    case 'list_dm': {
+            $list_dm = loadAll_danhmuc();
+            include "danhmuc/list_dm.php";
+            break;
+        }
+    case 'delete_dm': {
+            if (isset($_GET['id']) && ($_GET['id']) > 0) {
+                $loadone_dm = loadAll_danhmuc("", $_GET['id']);
+                delete_dm($_GET['id']);
+                echo "<script>alert('Xóa danh mục thành công');</script>";
+            }
+            $list_dm = loadAll_danhmuc();
+            include "danhmuc/list_dm.php";
+            break;
+        }
+    case 'update_dm': {
+            if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+                $loadone_dm = loadAll_danhmuc("", $_GET['id']);
+                if (isset($_POST['submit']) && ($_POST['submit'])) {
+                    $name = $_POST['name'];
+                    $listone_dm = loadAll_danhmuc($name, 0);
+                    update_dm($_GET['id'], $name, $_FILES['img']['name']);
+                    $thongbao = "Cap nhap danh mục thanh cong";
+                }
+                include "danhmuc/update_dm.php";
+                break;
+            }
+        }
         case "add_sp": {
                 if (isset($_POST['submit']) && ($_POST['submit'])) {
                     $name = $_POST['name'];
