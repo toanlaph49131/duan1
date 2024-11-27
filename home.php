86b10e45cb2bce463471a6442ffd307076c25e8a
toanLa <toananhle2k5@gmail.com>
2024-11-28 02:29:29 +0700

    code up cart

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Doanh thu</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"> ₫</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= number_format($thong_ke_doanh_thu['tong_thanhtien'], 0, ',', '.') ?> ₫</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -32,7 +32,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                 Số lượng sản phẩm</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $count_sp ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -59,6 +59,31 @@
                         <!-- Card Body -->
                         <div class="card-body">
                             <div class="chart-area">
+                                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                                <script type="text/javascript">
+                                    google.charts.load("current", {
+                                        packages: ["corechart"]
+                                    });
+                                    google.charts.setOnLoadCallback(drawChart);
+
+                                    function drawChart() {
+                                        var data = google.visualization.arrayToDataTable([
+                                            ['Doanh thu', 'Tháng'],
+                                            <?php foreach ($thong_ke_doanh_thu_thang as $value) {
+                                                extract($value);
+                                                echo "['$thang', $doanhthu],";
+                                            } ?>
+                                        ]);
+
+                                        var options = {
+                                            title: 'Biểu đồ thống kê doanh thu theo tháng',
+                                            is3D: true,
+                                        };
+
+                                        var chart = new google.visualization.ColumnChart(document.getElementById('piechart_4d'));
+                                        chart.draw(data, options);
+                                    }
+                                </script>
                                 <div id="piechart_4d" style="width: 100%; height: 350px;"></div>
                             </div>
                         </div>
@@ -68,6 +93,31 @@
                     <div class="card shadow mb-4">
                         <!-- Card Body -->
                         <div class="card card-body">
+                            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                            <script type="text/javascript">
+                                google.charts.load("current", {
+                                    packages: ["corechart"]
+                                });
+                                google.charts.setOnLoadCallback(drawChart);
+
+                                function drawChart() {
+                                    var data = google.visualization.arrayToDataTable([
+                                        ['Danh mục', 'Số lượng'],
+                                        <?php foreach ($thong_ke as $value) {
+                                            extract($value);
+                                            echo "['$name', $soluong_sanpham],";
+                                        } ?>
+                                    ]);
+
+                                    var options = {
+                                        title: 'Biểu đồ thống kê sản phẩm theo doanh mục',
+                                        is3D: true,
+                                    };
+
+                                    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+                                    chart.draw(data, options);
+                                }
+                            </script>
                             <div id="piechart_3d" style="width: 100%; height: 350px;"></div>
                         </div>
                     </div>
---
+++ model/cart.php
@@ -0,0 +1,43 @@
+<?php
+
+function loadAll_cart($iduser)
+{
+    $sql = "SELECT cart.id as idcart, sanpham.id as idsp, sanpham.name,sanpham.gia, sanpham.gia_new, sanpham.img, cart.soluong FROM sanpham JOIN cart ON cart.idsp = sanpham.id WHERE cart.iduser = '$iduser' order by cart.id desc";
+    return pdo_query($sql);
+}
+function insert_cart($iduser, $idsp, $soluong)
+{
+    $sql = "INSERT INTO `cart` (`iduser`, `idsp`, `soluong`) VALUES ('$iduser', '$idsp', '$soluong')";
+    pdo_execute($sql);
+}
+function count_cart($iduser)
+{
+    $sql = "SELECT * FROM cart WHERE iduser = '$iduser'";
+    return pdo_query($sql);
+}
+
+function delete_cart($id)
+{
+    $sql = "DELETE FROM cart WHERE id = '$id'";
+    pdo_execute($sql);
+}
+function update_sl($id, $idsp, $them)
+{
+    $sql = "UPDATE cart SET soluong = '$them' WHERE iduser = '$id' AND idsp = '$idsp'";
+    pdo_execute($sql);
+}
+function update_soluong($id, $idsp, $giam = "", $tang = "")
+{
+    $min_quantity = 1;
+    $max_quantity = 10;
+
+    if ($tang != "") {
+        $sql = "UPDATE cart SET soluong = LEAST(soluong + 1, $max_quantity) WHERE iduser = '$id' AND id = '$idsp'";
+        pdo_execute($sql);
+    }
+
+    if ($giam != "") {
+        $sql = "UPDATE cart SET soluong = GREATEST(soluong - 1, $min_quantity) WHERE iduser = '$id' AND id = '$idsp'";
+        pdo_execute($sql);
+    }
+}
